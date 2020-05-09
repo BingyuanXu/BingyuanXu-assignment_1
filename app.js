94b833397115f3133f2496971dc45edc8adc4ba5
Bingyuan Xu <xubingyuan123@hotmail.com>
2020-05-09 12:06:38 -0500

    add buildStreetList function which accepts a streetArray parameter and build a list of street names in html

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const form = document.querySelector(`form`);
 const input = form.querySelector(`input`);
 const  apiKey = `Ehg5Nso4pNe0kGRIfPW`;
+const streetContainer = document.querySelector(`.streets`);
+
 
 form.addEventListener(`keypress`, function (event) {
   if (event.keyCode === 13) {
@@ -21,4 +23,16 @@ fetch(`https://api.winnipegtransit.com/v3/streets.json?api-key=${apiKey}&name=${
 }).then(json => console.log(json.streets))
 }
 
-getStreet(`henlow`)
+function buildStreetList(streetArray) {
+  let html = ``;
+  streetContainer.innerHTML = ``;
+  if (streetArray !== []) {
+    streetArray.forEach(ele => {
+      html += `<a href="#" data-street-key="${ele.key}">${ele.name}</a>`;
+    });
+  } else {
+    html = `No Streets found`;
+  }
+
+  streetContainer.insertAdjacentHTML(`beforeend`, `${html}`)
+}
