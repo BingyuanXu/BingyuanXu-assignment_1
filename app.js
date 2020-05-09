315aff56975c9d89598121cf9bcabbdc5ab015a5
Bingyuan Xu <xubingyuan123@hotmail.com>
2020-05-09 00:16:21 -0500

    create a getStreet function which can output an array of matching streets related to the inputStName parameter

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const form = document.querySelector(`form`);
 const input = form.querySelector(`input`);
+const  apiKey = `Ehg5Nso4pNe0kGRIfPW`;
 
 form.addEventListener(`keypress`, function (event) {
   if (event.keyCode === 13) {
@@ -8,3 +9,9 @@ form.addEventListener(`keypress`, function (event) {
   }
 });
 
+
+function getStreet(inputStName){
+fetch(`https://api.winnipegtransit.com/v3/streets.json?api-key=${apiKey}&name=${inputStName}&usage=long`)
+.then(response => response.json())
+.then(json => console.log(json))
+}
