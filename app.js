8e50766b0e6c0a9aabc11175bf461ac86554aa4b
Bingyuan Xu <xubingyuan123@hotmail.com>
2020-05-11 19:35:26 -0500

    refactor the table part, make more reabable

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ form.addEventListener(`keypress`, function (event) {
 
 streetContainer.addEventListener(`click`, function (event) {
   if (event.target.tagName === `A`) {
-    stopInStreet(event.target.dataset.streetKey);
-    addStreetNameEle(event.target.textContent);
+    buildStreetNameHeader(event.target.textContent);
+    buildSchedualTable(event.target.dataset.streetKey);
   }
 })
 
@@ -24,7 +24,7 @@ function getStreet(inputStName) {
       if (response.ok) {
         return response.json();
       } else {
-        throw new Error("There is a problem in street name (;T__T:)");
+        throw new Error(`There is a problem in street name (;T__T:)`);
       }
     })
     .then(json => json.streets)
@@ -36,7 +36,7 @@ function buildStreetList(inputStName) {
 
   getStreet(inputStName)
     .then(streetArray => {
-      if (streetArray !== []) {
+      if (streetArray.length !== 0) {
         streetArray.forEach(ele => {
           html += `<a href="#" data-street-key="${ele.key}">${ele.name}</a>`;
         });
@@ -48,72 +48,78 @@ function buildStreetList(inputStName) {
     })
 }
 
-function addStreetNameEle(streetName) {
+function buildStreetNameHeader(streetName) {
   const streetNameEle = document.querySelector(`#street-name`);
   streetNameEle.textContent = ``;
   streetNameEle.textContent = `Displaying results for ${streetName}`;
 }
 
-function buildSchedualTable(scheduleArray) {
-  let html = '';
-  tableContainer.innerHTML = '';
+function buildSchedualTable(streetKey) {
+  let html = ``;
+  tableContainer.innerHTML = ``;
 
-  for (let schedule of scheduleArray) {
-    for (let routeSchedule of schedule[`route-schedules`]) {
-      html += ` <tr>
-      <td>${schedule.stop.street.name}</td>
-      <td>${schedule.stop[`cross-street`].name}</td>
-      <td>${schedule.stop.direction}</td>
-      <td>${routeSchedule.route.number}</td>
-      <td>${timeFormatter(routeSchedule[`scheduled-stops`][0].times.departure.estimated)}</td>
-    </tr>`;
-    }
-  }
+  scheduleArray(streetKey)
+    .then(scheduleArray => {
+      for (let schedule of scheduleArray) {
+        for (let routeSchedule of schedule[`route-schedules`]) {
+          html += ` <tr>
+                      <td>${schedule.stop.street.name}</td>
+                      <td>${schedule.stop[`cross-street`].name}</td>
+                      <td>${schedule.stop.direction}</td>
+                      <td>${routeSchedule.route.number}</td>
+                      <td>${timeFormatter(routeSchedule[`scheduled-stops`][0].times.departure.estimated)}</td>
+                    </tr>`;
+        }
+      }
 
-  tableContainer.insertAdjacentHTML(`beforeend`, html)
+      tableContainer.insertAdjacentHTML(`beforeend`, html)
+    })
 }
 
-function scheduleArrayPromise(stopArray) {
-  const stopKeyArray = stopArray.map(ele => ele.key);
+function scheduleArray(streetKey) {
   const jsonPromise = [];
 
-  for (let stopKey of stopKeyArray) {
-    jsonPromise.push(
-      fetch(`https://api.winnipegtransit.com/v3/stops/${stopKey}/schedule.json?api-key=Ehg5Nso4pNe0kGRIfPW&max-results-per-route=2`)
-        .then(response => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            throw new Error("There is a problem in stop names (;T__T:)");
-          }
-        })
-        .then(json => json[`stop-schedule`])
-    )
-  }
+  return stopsInStreet(streetKey)
+    .then(stopArray => {
+      const stopKeyArray = stopArray.map(ele => ele.key);
 
-  Promise.all(jsonPromise)
-    .then(result => buildSchedualTable(result))
+      for (let stopKey of stopKeyArray) {
+        jsonPromise.push(
+          fetch(`https://api.winnipegtransit.com/v3/stops/${stopKey}/schedule.json?api-key=${apiKey}&max-results-per-route=2`)
+            .then(response => {
+              if (response.ok) {
+                return response.json();
+              } else {
+                throw new Error(`There is a problem in stop names (;T__T:)`);
+              }
+            })
+            .then(json => json[`stop-schedule`])
+        )
+      }
+
+      return Promise.all(jsonPromise)
+    })
 }
 
-function stopInStreet(streetKey) {
-  fetch(`https://api.winnipegtransit.com/v3/stops.json?street=${streetKey}&api-key=${apiKey}`)
+function stopsInStreet(streetKey) {
+  return fetch(`https://api.winnipegtransit.com/v3/stops.json?street=${streetKey}&api-key=${apiKey}`)
     .then(response => {
       if (response.ok) {
         return response.json();
       } else {
-        throw new Error("There is a problem in stop names (;T__T:)");
+        throw new Error(`There is a problem in stop names (;T__T:)`);
       }
     })
-    .then(json => scheduleArrayPromise(json.stops))
+    .then(json => json.stops)
 }
 
 function timeFormatter(timeString) {
   const time = new Date(timeString);
   const options = {
-    timeZone: "Canada/Central",
+    timeZone: `Canada/Central`,
     hour12: true,
-    hour: "numeric",
-    minute: "numeric", seconds: "numeric"
+    hour: `numeric`,
+    minute: `numeric`,
   }
   return time.toLocaleTimeString(`en-US`, options);
 }
