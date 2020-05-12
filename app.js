5d4c0328b25de7bfbc38c7269dfab829425fb2dd
Bingyuan Xu <xubingyuan123@hotmail.com>
2020-05-11 19:43:17 -0500

    indentation

--- app.js
+++ app.js
@@ -9,43 +9,44 @@ form.addEventListener(`keypress`, function (event) {
     event.preventDefault();
     buildStreetList(input.value);
   }
-})
+});
 
 streetContainer.addEventListener(`click`, function (event) {
   if (event.target.tagName === `A`) {
     buildStreetNameHeader(event.target.textContent);
     buildSchedualTable(event.target.dataset.streetKey);
   }
-})
+});
 
 function getStreetName(inputStName) {
-  return fetch(`https://api.winnipegtransit.com/v3/streets.json?api-key=${apiKey}&name=${inputStName}&usage=long`)
-    .then(response => {
+  return fetch(
+    `https://api.winnipegtransit.com/v3/streets.json?api-key=${apiKey}&name=${inputStName}&usage=long`
+  )
+    .then((response) => {
       if (response.ok) {
         return response.json();
       } else {
         throw new Error(`There is a problem in street name (;T__T:)`);
       }
     })
-    .then(json => json.streets)
+    .then((json) => json.streets);
 }
 
 function buildStreetList(inputStName) {
   let html = ``;
   streetContainer.innerHTML = ``;
 
-  getStreetName(inputStName)
-    .then(streetArray => {
-      if (streetArray.length !== 0) {
-        streetArray.forEach(ele => {
-          html += `<a href="#" data-street-key="${ele.key}">${ele.name}</a>`;
-        });
-      } else {
-        html = `No Streets found`;
-      }
+  getStreetName(inputStName).then((streetArray) => {
+    if (streetArray.length !== 0) {
+      streetArray.forEach((ele) => {
+        html += `<a href="#" data-street-key="${ele.key}">${ele.name}</a>`;
+      });
+    } else {
+      html = `No Streets found`;
+    }
 
-      streetContainer.insertAdjacentHTML(`beforeend`, html)
-    })
+    streetContainer.insertAdjacentHTML(`beforeend`, html);
+  });
 }
 
 function buildStreetNameHeader(streetName) {
@@ -58,59 +59,64 @@ function buildSchedualTable(streetKey) {
   let html = ``;
   tableContainer.innerHTML = ``;
 
-  getScheduleArray(streetKey)
-    .then(scheduleArray => {
-      for (let schedule of scheduleArray) {
-        for (let routeSchedule of schedule[`route-schedules`]) {
-          html += ` <tr>
+  getScheduleArray(streetKey).then((scheduleArray) => {
+    for (let schedule of scheduleArray) {
+      for (let routeSchedule of schedule[`route-schedules`]) {
+        html += ` <tr>
                       <td>${schedule.stop.street.name}</td>
                       <td>${schedule.stop[`cross-street`].name}</td>
                       <td>${schedule.stop.direction}</td>
                       <td>${routeSchedule.route.number}</td>
-                      <td>${timeFormatter(routeSchedule[`scheduled-stops`][0].times.departure.estimated)}</td>
+                      <td>${timeFormatter(
+                        routeSchedule[`scheduled-stops`][0].times.departure
+                          .estimated
+                      )}</td>
                     </tr>`;
-        }
       }
+    }
 
-      tableContainer.insertAdjacentHTML(`beforeend`, html)
-    })
+    tableContainer.insertAdjacentHTML(`beforeend`, html);
+  });
 }
 
 function getScheduleArray(streetKey) {
   const jsonPromise = [];
 
-  return gstStopNamesInStreet(streetKey)
-    .then(stopArray => {
-      const stopKeyArray = stopArray.map(ele => ele.key);
+  return gstStopNamesInStreet(streetKey).then((stopArray) => {
+    const stopKeyArray = stopArray.map((ele) => ele.key);
 
-      for (let stopKey of stopKeyArray) {
-        jsonPromise.push(
-          fetch(`https://api.winnipegtransit.com/v3/stops/${stopKey}/schedule.json?api-key=${apiKey}&max-results-per-route=2`)
-            .then(response => {
-              if (response.ok) {
-                return response.json();
-              } else {
-                throw new Error(`There is a problem in stop names (;T__T:)`);
-              }
-            })
-            .then(json => json[`stop-schedule`])
+    for (let stopKey of stopKeyArray) {
+      jsonPromise.push(
+        fetch(
+          `https://api.winnipegtransit.com/v3/stops/${stopKey}/schedule.json?api-key=${apiKey}&max-results-per-route=2`
         )
-      }
+          .then((response) => {
+            if (response.ok) {
+              return response.json();
+            } else {
+              throw new Error(`There is a problem in stop names (;T__T:)`);
+            }
+          })
+          .then((json) => json[`stop-schedule`])
+      );
+    }
 
-      return Promise.all(jsonPromise)
-    })
+    return Promise.all(jsonPromise);
+  });
 }
 
 function gstStopNamesInStreet(streetKey) {
-  return fetch(`https://api.winnipegtransit.com/v3/stops.json?street=${streetKey}&api-key=${apiKey}`)
-    .then(response => {
+  return fetch(
+    `https://api.winnipegtransit.com/v3/stops.json?street=${streetKey}&api-key=${apiKey}`
+  )
+    .then((response) => {
       if (response.ok) {
         return response.json();
       } else {
         throw new Error(`There is a problem in stop names (;T__T:)`);
       }
     })
-    .then(json => json.stops)
+    .then((json) => json.stops);
 }
 
 function timeFormatter(timeString) {
@@ -120,6 +126,6 @@ function timeFormatter(timeString) {
     hour12: true,
     hour: `numeric`,
     minute: `numeric`,
-  }
+  };
   return time.toLocaleTimeString(`en-US`, options);
 }
